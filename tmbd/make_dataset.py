f176d22732cafddf1a4152b768fe3e621ab30e53
Timothy Eldridge <tim@alder.local>
2019-06-02 00:32:18 -0700

    add directory for figures

--- tmbd/make_dataset.py
+++ tmbd/make_dataset.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 # CAUTION: this breaks when installed in site-packages.
-ROOT_DIR = Path(__file__).parent.parent
-RAW_DATA_DIR = ROOT_DIR.joinpath("data/raw")
-PROCESSED_DATA_DIR = ROOT_DIR.joinpath("data/raw")
+ROOT_DIR = Path(__file__).parent.parent            # Project root
+RAW_DATA_DIR       = ROOT_DIR.joinpath("data/raw") # Directory for raw data
+PROCESSED_DATA_DIR = ROOT_DIR.joinpath("data/raw") # Directory for processed data
+FIG_DIR            = ROOT_DIR.joinpath("figures")  # Directory for figures
